e2e29108e93fe015df6d94c35e806f7e39cc2dc8
DmitryArhipov <nevi_dimka1@mail.ru>
2020-04-24 18:10:25 +0500

    Тестим реквест

--- Bomberman/Program.cs
+++ Bomberman/Program.cs
@@ -11,6 +11,7 @@ namespace Bomberman
         [STAThread]
         static void Main()
         {
+            //hay
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Window());
