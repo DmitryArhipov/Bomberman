26f12345145a463193842f699d45a211c85c3aa5
DmitryArhipov <nevi_dimka1@mail.ru>
2020-04-26 12:24:36 +0500

    Логика игры

---
+++ Bomberman/Game.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Forms;
+
+namespace Bomberman
+{
+    public static class Game
+    {
+        private const string MapExample = "P"; // убрать
+
+        public static ICreature[,] Map;
+        public static bool IsOver;
+
+        public static Keys KeyPressed;
+        public static int MapWidth => Map.GetLength(0);
+        public static int MapHeight => Map.GetLength(1);
+
+        public static void CreateMap()
+        {
+            Map = MapParser.GetMapFromText(MapExample);
+        }
+    }
+}
