b3978e93fb9d2f33f092f2f5ef3eda12603f9bf8
Darya V <dashav1605@mail.ru>
2020-05-06 09:45:34 +0500

    Parser отвалился в предыдущем коммите.

--- Bomberman/MapParser.cs
+++ Bomberman/MapParser.cs
@@ -12,16 +12,20 @@ namespace Bomberman
             var map = new ICreature[lines[0].Length, lines.Length][];
             for (var y = 0; y < lines.Length; y++)
             for (var x = 0; x < lines[0].Length; x++)
+            {
+                if (lines[y][x] == 'M') 
+                    Game.MonstersCount++; // Будет полем уровня, скорее всего, а не Гейм
                 map[x, y] = lines[y][x] switch
                 {
                     'P' => Helpers.Array<Player>(),
                     'W' => Helpers.Array<BreakableWall>(),
                     '#' => Helpers.Array<UnbreakableWall>(),
                     'M' => Helpers.Array<PredictableMonster>(),
-                    'B' => new ICreature[] {new Bomb(null)},
                     ' ' => new ICreature[] { },
                     _ => throw new Exception($"wrong character for map {lines[y][x]}")
                 };
+            }
+                
             return map;
         }
     }
