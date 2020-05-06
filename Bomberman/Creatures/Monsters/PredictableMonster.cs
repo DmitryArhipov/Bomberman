ad645024c04d5fec1ea3c9c2a14f705d855531de
DmitryArhipov <nevi_dimka1@mail.ru>
2020-05-07 00:21:39 +0500

    Баг пофикшен)

--- Bomberman/Creatures/Monsters/PredictableMonster.cs
+++ Bomberman/Creatures/Monsters/PredictableMonster.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Drawing;
 
 namespace Bomberman
 {
@@ -7,45 +8,64 @@ namespace Bomberman
     {
         private int direction;
         private Stopwatch timer = Stopwatch.StartNew();
+        private Point monsterCell;
+        public const double secondsBeforeGo = 1;
         public string GetImageFileName() => "PredictableMonster.png";
 
         public CreatureCommand Act(int x, int y)
         {
             var result = new CreatureCommand();
+            monsterCell = new Point(x, y);
             switch (direction)
             {
-                case 0 when x + 1 < Game.MapWidth && !Game.Map[x + 1, y].ContainsWallOrBomb() && !Game.Map[x + 1, y].ContainsMonster():
+                case 0 when x + 1 < Game.MapWidth && !Game.Map[x + 1, y].ContainsWallOrBomb()
+                                                  && !Game.Map[x + 1, y].ContainsMonster() && Game.WantToMoveMonster[x + 1, y] == false:
                 {
-                    if (timer.Elapsed >= TimeSpan.FromSeconds(1))
+                    if (timer.Elapsed >= TimeSpan.FromSeconds(secondsBeforeGo))
                     {
                         timer = Stopwatch.StartNew();
+                        Game.WantToMoveMonster[x, y] = false;
+                        monsterCell.X++;
+                        Game.WantToMoveMonster[x + 1, y] = true;
                         result.DeltaX = 1;
                     }
                     break;
                 }
-                case 1 when y + 1 < Game.MapHeight && !Game.Map[x, y + 1].ContainsWallOrBomb() && !Game.Map[x, y + 1].ContainsMonster():
+                case 1 when y + 1 < Game.MapHeight && !Game.Map[x, y + 1].ContainsWallOrBomb()
+                                                   && !Game.Map[x, y + 1].ContainsMonster() && Game.WantToMoveMonster[x, y + 1] == false:
                 {
-                    if (timer.Elapsed >= TimeSpan.FromSeconds(1))
+                    if (timer.Elapsed >= TimeSpan.FromSeconds(secondsBeforeGo))
                     {
                         timer = Stopwatch.StartNew();
+                        Game.WantToMoveMonster[x, y] = false;
+                        monsterCell.Y++;
+                        Game.WantToMoveMonster[x, y + 1] = true;
                         result.DeltaY = 1;
                     }
                     break;
                 }
-                case 2 when x > 0 && !Game.Map[x - 1, y].ContainsWallOrBomb() && !Game.Map[x - 1, y].ContainsMonster():
+                case 2 when x > 0 && !Game.Map[x - 1, y].ContainsWallOrBomb()
+                                  && !Game.Map[x - 1, y].ContainsMonster() && Game.WantToMoveMonster[x - 1, y] == false:
                 {
-                    if (timer.Elapsed >= TimeSpan.FromSeconds(1))
+                    if (timer.Elapsed >= TimeSpan.FromSeconds(secondsBeforeGo))
                     {
                         timer = Stopwatch.StartNew();
+                        Game.WantToMoveMonster[x, y] = false;
+                        monsterCell.X--;
+                        Game.WantToMoveMonster[x - 1, y] = true;
                         result.DeltaX = -1;
                     }
                     break;
                 }
-                case 3 when y > 0 && !Game.Map[x, y - 1].ContainsWallOrBomb() && !Game.Map[x, y - 1].ContainsMonster():
+                case 3 when y > 0 && !Game.Map[x, y - 1].ContainsWallOrBomb()
+                                  && !Game.Map[x, y - 1].ContainsMonster() && Game.WantToMoveMonster[x, y - 1] == false:
                 {
-                    if (timer.Elapsed >= TimeSpan.FromSeconds(1))
+                    if (timer.Elapsed >= TimeSpan.FromSeconds(secondsBeforeGo))
                     {
                         timer = Stopwatch.StartNew();
+                        Game.WantToMoveMonster[x, y] = false;
+                        monsterCell.Y--;
+                        Game.WantToMoveMonster[x, y - 1] = true;
                         result.DeltaY = -1;
                     }
                     break;
@@ -57,7 +77,11 @@ namespace Bomberman
             return result;
         }
 
-        public bool DeadInConflict(ICreature conflictedObject) => conflictedObject is Fire;
+        public bool DeadInConflict(ICreature conflictedObject)
+        {
+            Game.WantToMoveMonster[monsterCell.X, monsterCell.Y] = false;
+            return conflictedObject is Fire;
+        }
 
         public int GetDrawingPriority() => 3;
     }
--- Bomberman/Logic/Game.cs
+++ Bomberman/Logic/Game.cs
@@ -25,11 +25,13 @@ namespace Bomberman
         public static Keys KeyPressed;
         public static int MapWidth => Map.GetLength(0);
         public static int MapHeight => Map.GetLength(1);
+        public static bool[,] WantToMoveMonster;
         public static int MonstersCount;
 
         public static void CreateMap(string map)
         {
             Map = MapParser.GetMapFromText(map);
+            WantToMoveMonster = new bool[MapWidth, MapHeight];
         }
     }
 }
