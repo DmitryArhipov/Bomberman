a2377bd11ee8fad8e1171c11fb3a229598e23d1b
DmitryArhipov <nevi_dimka1@mail.ru>
2020-05-23 03:06:45 +0500

    Починил блок

--- Bomberman/Creatures/Obstacles/Block.cs
+++ Bomberman/Creatures/Obstacles/Block.cs
@@ -13,35 +13,38 @@
         public CreatureCommand Act(int x, int y)
         {
             var result = new CreatureCommand();
-            if(hit)
+            if (hit)
+            {
+                hit = false;
                 switch (direction)
                 {
                     case Direction.Up:
                     {
-                        if(y > 0 && !Game.Map[x, y - 1].ContainsObstaclesOrBomb()) 
+                        if (y > 0 && !Game.Map[x, y - 1].ContainsObstaclesOrBomb())
                             result.DeltaY = -1;
                         break;
                     }
                     case Direction.Down:
                     {
-                        if(y + 1 < Game.MapHeight && !Game.Map[x, y + 1].ContainsObstaclesOrBomb()) 
+                        if (y + 1 < Game.MapHeight && !Game.Map[x, y + 1].ContainsObstaclesOrBomb())
                             result.DeltaY = 1;
                         break;
                     }
                     case Direction.Right:
                     {
-                        if(x + 1 < Game.MapWidth && !Game.Map[x + 1, y].ContainsObstaclesOrBomb()) 
+                        if (x + 1 < Game.MapWidth && !Game.Map[x + 1, y].ContainsObstaclesOrBomb())
                             result.DeltaX = 1;
                         break;
                     }
                     default:
                     {
-                        if(x > 0 && !Game.Map[x - 1, y].ContainsObstaclesOrBomb()) 
+                        if (x > 0 && !Game.Map[x - 1, y].ContainsObstaclesOrBomb())
                             result.DeltaX = -1;
                         break;
                     }
                 }
-            
+            }
+
             return result;
         }
 
