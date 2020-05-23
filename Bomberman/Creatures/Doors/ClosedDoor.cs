a1cb4182db7463b37f1be3a15c6ce473ab7be60d
Darya V <dashav1605@mail.ru>
2020-05-24 00:41:58 +0500

    Еще небольшая чистка.

--- Bomberman/Creatures/Doors/ClosedDoor.cs
+++ Bomberman/Creatures/Doors/ClosedDoor.cs
@@ -2,20 +2,10 @@
 {
     public class ClosedDoor : ICreature
     {
-        private bool fireHit;
         public string GetImageFileName() => "ClosedDoor.png";
 
         public CreatureCommand Act(int x, int y)
         {
-            if (fireHit)
-            {
-                fireHit = false;
-                return
-                    new CreatureCommand { TransformTo =
-                        new ICreature[] {
-                            new ClosedDoor(),
-                            new PredictableMonster() } };
-            }
             return Game.MonstersCount == 0 
                 ? new CreatureCommand { TransformTo = new[] { new OpenDoor() } } 
                 : new CreatureCommand();
